8308299d8f8c41c447ad9ad47d102cd37a6f491d
Fahad Kiani <fahad@jedilabs.org>
2026-07-31 00:10:15 +0000

    Restore package.json, package-lock.json, remotion/, next.config.ts, tsconfig.json from last successful build

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: [
+    '@remotion/renderer',
+    '@remotion/bundler',
+    'remotion',
+  ],
+
   images: {
     remotePatterns: [
       {
